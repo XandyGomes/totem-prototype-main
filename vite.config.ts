8488197b59f64214fb8145e95ea324520dc3afe4
XandyGomes <x3.computadores@hotmail.com>
2025-11-13 17:29:05 -0300

    ajustes.13.11

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path"; 
+import path from "path";
 import { componentTagger } from "lovable-tagger";
-
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -10,19 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  // plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  // resolve: {
-  //   alias: {
-  //     "@": path.resolve(__dirname, "./src"),
-  //   },
-  // },
-  plugins: [react()],
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/totem-prototype-main/' // seu repositório
- 
-
 }));
